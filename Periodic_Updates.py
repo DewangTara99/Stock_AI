20454fa10c27e28129fdfa86fe59e2a7b728ed0b
Dewang Tara <dewangtara@gmail.com>
2021-03-15 21:24:07 -0400

    Update Periodic_Updates.py

--- Periodic_Updates.py
+++ Periodic_Updates.py
@@ -5,13 +5,18 @@ import math
 import pandas_datareader as web
 import datetime
 import os
+from Parameters import Parameters
+import csv
+import yfinance as yf
 
 # updates stock's csv files
 def Update_CSV(ticker):
     start = datetime.datetime(2010, 1, 1)
     end = datetime.datetime.now()
 
-    df = web.DataReader(ticker, 'yahoo', start, end)
+    #df = web.DataReader(ticker, 'yahoo', start, end)
+    df = yf.download(ticker, start, end)
+
     path = r'/Users/Dewangtara/Desktop/Stock_AI'
     CSVstring = str(ticker) + '.csv'
     df.to_csv(os.path.join(path, CSVstring))
